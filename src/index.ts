43b2ce0ec4804307e1b242cfc0719f5f0a188b74
Adi-Samudra <adityasamudra369@gmail.com>
2025-02-16 00:00:01 +0530

    test ai gateway

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 export interface Env {
-    // Add any environment variables here
+    api_token: string;
 }
 
-const api_token = "AIzaSyC3dD0HMAGDXzXC2Z8OmPrfGjLoqacjrrc";
 
-const account_id = "b0e6a5b1c1eac4f367363b71681bd4b8";
-const gateway_name = "firstgateway";
 
-const genAI = new GoogleGenerativeAI(api_token);
-const model = genAI.getGenerativeModel(
-    { model: "gemini-2.0-pro-exp-02-05" },
-    {
-        baseUrl: `https://gateway.ai.cloudflare.com/v1/${account_id}/${gateway_name}/google-ai-studio`,
-    },
-);
+
 
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -28,11 +19,22 @@ export default {
             });
         }
 
-        if (request.method === 'POST') {
+        if (request.method === 'GET') {
             // Here you would typically call an AI service to generate the outfit description
             // For this example, we'll just return a mock response
             // const outfitDescription = `A beautiful ${input} inspired Indian ethnic outfit with intricate embroidery and vibrant colors.`
-            
+            const api_token = env.api_token;
+            const account_id = "b0e6a5b1c1eac4f367363b71681bd4b8";
+            const gateway_name = "firstgateway";
+
+            const genAI = new GoogleGenerativeAI(api_token);
+            const model = genAI.getGenerativeModel(
+                { model: "gemini-2.0-pro-exp-02-05" },
+                {
+                    baseUrl: `https://gateway.ai.cloudflare.com/v1/${account_id}/${gateway_name}/google-ai-studio`,
+                },
+            );
+
             return new Response(JSON.stringify(await model.generateContent(["What is Cloudflare?"])), {
                 headers: {
                     'Content-Type': 'application/json',
